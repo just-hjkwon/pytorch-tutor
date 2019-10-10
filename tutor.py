f85605f58cecc01487cac03618f09015423aa174
just-hjkwon <just.hjkwon@gmail.com>
2019-10-10 11:43:32 +0900

    [#1] Implement tutor.

--- tutor.py
+++ tutor.py
@@ -1,3 +1,6 @@
+import os
+
+import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.optim.lr_scheduler as lr_scheduler
@@ -71,3 +74,63 @@ class Tutor:
         loss = self.model.criticize(prediction, target)
 
         return loss.data, prediction.data
+
+    def save(self, prefix):
+        if not os.path.exists(self.snapshot_directory):
+            os.mkdir(self.snapshot_directory)
+
+        self.model.eval()
+
+        filename = os.path.join(self.snapshot_directory, "%s.weights" % prefix)
+        torch.save(self.model.state_dict(), filename)
+
+        filename = os.path.join(self.snapshot_directory, "%s.optimizer" % prefix)
+        torch.save(self.optimizer.state_dict(), filename)
+
+        filename = os.path.join(self.snapshot_directory, "%s.scheduler" % prefix)
+        torch.save(self.scheduler.state_dict(), filename)
+
+        train_state = {'best_error': self.best_error, 'epoch': self.epoch}
+        filename = os.path.join(self.snapshot_directory, "%s.state" % prefix)
+        torch.save(train_state, filename)
+
+    def load(self, prefix):
+        self.model.eval()
+
+        filename = os.path.join(self.snapshot_directory, "%s.weights" % prefix)
+        self.model.load_state_dict(torch.load(filename))
+
+        filename = os.path.join(self.snapshot_directory, "%s.optimizer" % prefix)
+        self.optimizer.load_state_dict(torch.load(filename))
+
+        filename = os.path.join(self.snapshot_directory, "%s.scheduler" % prefix)
+        self.scheduler.load_state_dict(torch.load(filename))
+
+        filename = os.path.join(self.snapshot_directory, "%s.state" % prefix)
+        train_state = torch.load(filename)
+
+        self.best_error = train_state['best_acer']
+        self.epoch = train_state['epoch']
+
+    def start_new_epoch(self):
+        self.epoch += 1
+
+    def end_epoch(self, validation_loss):
+        self.scheduler.step(validation_loss)
+
+    def get_current_learning_rate(self):
+        learning_rate = 0.0
+
+        for param_group in self.optimizer.param_groups:
+            learning_rate = param_group['lr']
+
+        return learning_rate
+
+    def make_scheduler_state_string(self):
+        state_string = "Best Error: %f, Best loss: %f, Scheduler patience: %d/%d" % (
+            self.best_error, self.scheduler.best, self.scheduler.num_bad_epochs, self.scheduler.patience)
+
+        return state_string
+
+    def epoch(self):
+        return self.epoch
