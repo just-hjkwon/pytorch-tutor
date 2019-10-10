04df20732c0c24b7a6207317c424c3da2bcfa143
just-hjkwon <just.hjkwon@gmail.com>
2019-10-10 22:32:28 +0900

    [#1] Fix error.

--- dummy_model.py
+++ dummy_model.py
@@ -5,10 +5,15 @@ import torch.nn as nn
 class DummyModel(nn.Module):
     def __init__(self):
         super(DummyModel, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=2, kernel_size=(100, 100), padding=0)
+
         self.loss = self.create_loss()
 
     def forward(self, input):
-        return torch.sum(input * 2.0, dim=(1, 2, 3))
+        output = self.conv1(input)
+        output = output.squeeze()
+        return output
 
     def create_loss(self):
         return nn.CrossEntropyLoss()
