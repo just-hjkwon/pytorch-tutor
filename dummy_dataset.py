5d6b71aa9f1ea649af160b3b8c3d4607ffb7ac3d
just-hjkwon <just.hjkwon@gmail.com>
2019-10-10 13:55:31 +0900

    [#1] Fix error.

--- dummy_dataset.py
+++ dummy_dataset.py
@@ -13,7 +13,7 @@ class DummyDataset(Dataset):
         return 1024
 
     def __getitem__(self, item):
-        input = np.random.rand(3, 100, 100, dtype=np.dtype.float32)
+        input = np.random.rand(3, 100, 100).astype(dtype=np.float32)
         input = torch.from_numpy(input)
 
         label = np.random.random_integers(0, 2)
