5e0a02172fdc98a4700c42e1565f108807c6c0aa
just-hjkwon <just.hjkwon@gmail.com>
2019-10-10 13:25:02 +0900

    [#1] Implement loss.

--- dummy_model.py
+++ dummy_model.py
@@ -5,6 +5,14 @@ import torch.nn as nn
 class DummyModel(nn.Module):
     def __init__(self):
         super(DummyModel, self).__init__()
+        self.loss = self.create_loss()
 
     def forward(self, input):
         return torch.sum(input * 2.0, dim=(1, 2, 3))
+
+    def create_loss(self):
+        return nn.CrossEntropyLoss()
+
+    def criticize(self, prediction, target):
+        loss = self.loss(prediction, target)
+        return loss
